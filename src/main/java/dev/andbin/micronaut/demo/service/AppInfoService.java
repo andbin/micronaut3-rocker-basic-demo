162b42a0813504c1645ebc41502e5d9f5d16a083
andbin <andbin@users.noreply.github.com>
2022-09-10 15:10:01 +0200

    Add Netty versions informations displayed on the home page

--- src/main/java/dev/andbin/micronaut/demo/service/AppInfoService.java
+++ src/main/java/dev/andbin/micronaut/demo/service/AppInfoService.java
@@ -5,6 +5,9 @@ SPDX-License-Identifier: MIT
 
 package dev.andbin.micronaut.demo.service;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 import dev.andbin.micronaut.demo.model.AppInfo;
 import jakarta.inject.Singleton;
 
@@ -15,6 +18,7 @@ public class AppInfoService {
         appInfo.setJavaRuntimeVersion(getJavaRuntimeVersion());
         appInfo.setMicronautVersion(getMicronautVersion());
         appInfo.setRockerVersion(getRockerVersion());
+        appInfo.setNettyVersions(getNettyVersions());
         return appInfo;
     }
 
@@ -28,5 +32,15 @@ public class AppInfoService {
 
     private String getRockerVersion() {
         return com.fizzed.rocker.Version.getVersion();
+    }
+
+    private Map<String, String> getNettyVersions() {
+        var versions = new TreeMap<String, String>();
+
+        for (var entry : io.netty.util.Version.identify().entrySet()) {
+            versions.put(entry.getKey(), entry.getValue().artifactVersion());
+        }
+
+        return versions;
     }
 }
